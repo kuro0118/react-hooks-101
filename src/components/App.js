c4611dae7de3f3f0dba48ede474250e46d7308ce
iroha0118 <ilovegumi0118@gmail.com>
2021-04-01 21:28:26 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 
 // chips-bootstrap: import "bootstrap"とすると、JQueryが必要なモノまでインポートしてしまうので、
 //                  今回の演習ではJQueryは入れていない為、エラーが起きる。
 import "bootstrap/dist/css/bootstrap.min.css"
 
+
+// chips: ファイル指定しなくてもreducerをインポートできる。
+import reducer from '../reducers';
+
 // chips-bootstrap: container-fluidを指定すると、画面いっぱいにスタイルが適用される
 //                  conteinerを指定すると、左右に余白を残しつつ適用される。
 // chips-bootstrap: label、inputは同じidにすること！(ラベルを押すと、フォームにカーソルが作るのが正しい)
-
 const App = () => {
+  // chips: 第1引数：reducer、第2引数は扱いたいステート(今回は配列型のステートを使うため、空配列)
+  // chips: useStateの戻り値は第1戻り値がステート、第2戻り値がステートを変更させる為の関数 (復習)
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  // chips: 何もブロックに書いていないと、ボタンが押下された時はただ単に画面更新(リロード)だけになる。
+  //        >> e.preventDefault()と書くと、画面全体のリロードが行わなくなる。(SPAとしては必須の関数！)
+  const addEvent = e => {
+    e.preventDefault();
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('');
+    setBody('');
+  }
+
+  console.log({state});
+
+  // chips: e.target.valueにて、入力値を拾うことが出来る。
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディ</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
